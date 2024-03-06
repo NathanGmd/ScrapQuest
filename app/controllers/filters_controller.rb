cf30cc87b5d04196cef1762a1182da2fc06fc4d4
Nathan Germond <germond.nathan@gmail.com>
2024-03-06 12:16:27 +0100

    index first

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,16 +1,20 @@
 class FiltersController < ApplicationController
   def index
+    @research = Research.find(params[:research_id])
+    @filters = Filter.where(research_id: @research.id)
     @features = Feature.all
     @filter = Filter.new
   end
 
   def create
+    @research = Research.find(params[:research_id])
     @filter = Filter.new(
       option: Option.find_by(value: filter_params["option"]),
-      feature: Feature.find(Option.find_by(value: filter_params["option"]).feature_id)
+      feature: Feature.find(Option.find_by(value: filter_params["option"]).feature_id),
+      research: Research.find(params[:research_id])
     )
     @filter.save!
-    redirect_to filters_path, notice: "successfully added!"
+    redirect_to research_filters_path(@research), notice: "successfully added!"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   get 'researches/index'
   devise_for :users
   root to: "pages#home"
-  resources :filters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :researches
-  resources :items
-  resources :features
-  resources :prospects
+  resources :researches do
+    resources :filters
+  end
+
 
   # Defines the root path route ("/")
   # root "posts#index"
