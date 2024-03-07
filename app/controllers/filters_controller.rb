31ca35326eeb75dcb31eb40ad1950cff3a9183b6
Nathan Germond <germond.nathan@gmail.com>
2024-03-07 16:20:09 +0100

    create

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,7 +1,8 @@
 class FiltersController < ApplicationController
+  before_action :set_research, only: %i[index create]
+
   def index
     @filter = Filter.new
-    @research = Research.find(params[:research_id])
     @filters = Filter.where(research_id: @research)
     @features = Feature.all
     @grouped_options = @research.options.group_by(&:feature_id)
@@ -9,19 +10,31 @@ class FiltersController < ApplicationController
   end
 
   def create
-    @research = Research.find(params[:research_id])
     @filter = Filter.new(
       option: Option.find_by(value: filter_params["option"]),
       feature: Feature.find(Option.find_by(value: filter_params["option"]).feature_id),
       research: Research.find(params[:research_id])
     )
     @filter.save!
-    redirect_to research_filters_path(@research), notice: "successfully added!"
+    @grouped_options = @research.options.group_by(&:feature_id)
+    @grouped_options = @grouped_options.transform_keys! { |key| Feature.find(key) }
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'filters/filters_content', locals: { research: @research, grouped_options: @grouped_options }, formats: [:html] }
+    end
+  end
+
+  def delete
+    
   end
 
   private
 
   def filter_params
     params.require(:filter).permit(:option)
+  end
+
+  def set_research
+    @research = Research.find(params[:research_id])
   end
 end
